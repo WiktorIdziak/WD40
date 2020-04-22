db73302edc270de9fb114e1d4bb9b39677319c54
WiktorIdziak <Witusaki@gmail.com>
2020-04-22 23:23:39 +0200

    Delete zad3.py

--- zad3.py
+++
@@ -1,6 +0,0 @@
-print("Oryginalny słownik")
-towary = {"mleko": "litry", "mentosy": "sztuki", "krówki": "gramy", "parówki": "sztuki"}
-print(towary)
-print("sztuki")
-sztuki = {key: value for key, value in towary.items() if value == "sztuki"}
-print(sztuki)
